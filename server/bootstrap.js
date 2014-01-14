d4634c6c2b63174308f5739777dee304e23aa050
Chris Butler <chris@platformthirteen.com>
2014-01-14 12:24:13 -0500

    refactor screens and shows

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -18,82 +18,82 @@ if (Meteor.isServer) {
 var showList = [
   {
     "name": "default",
-    "main": "mp4",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/default/main.mp4",
+    "one": "http://hedgviz.s3.amazonaws.com/default/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/default/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/default/three.mp4",
     "text": "www.hedgeye.com"
   },
   {
     "name": "financials",
-    "main": "jpg",
-    "one": "jpg",
-    "two": "jpg",
-    "three": "jpg",
+    "main": "http://hedgviz.s3.amazonaws.com/financials/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/financials/one.jpg",
+    "two": "http://hedgviz.s3.amazonaws.com/financials/two.jpg",
+    "three": "http://hedgviz.s3.amazonaws.com/financials/three.jpg",
     "text": "financials"
   },
   {
     "name": "macro",
-    "main": "mp4",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/macro/main.mp4",
+    "one": "http://hedgviz.s3.amazonaws.com/macro/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/macro/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/macro/three.mp4",
     "text": "macro"
   },
   {
     "name": "gaming",
-    "main": "jpg",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/gaming/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/gaming/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/gaming/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/gaming/three.mp4",
     "text": "gaming"
   },
   {
     "name": "gold",
-    "main": "mp4",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/gold/main.mp4",
+    "one": "http://hedgviz.s3.amazonaws.com/gold/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/gold/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/gold/three.mp4",
     "text": "gold"
   },
   {
     "name": "healthcare",
-    "main": "jpg",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/healthcare/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/healthcare/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/healthcare/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/healthcare/three.mp4",
     "text": "healthcare"
   },
   {
     "name": "hedgetohedge",
-    "main": "jpg",
-    "one": "jpg",
-    "two": "jpg",
-    "three": "jpg",
+    "main": "http://hedgviz.s3.amazonaws.com/hedgetohedge/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/hedgetohedge/one.jpg",
+    "two": "http://hedgviz.s3.amazonaws.com/hedgetohedge/two.jpg",
+    "three": "http://hedgviz.s3.amazonaws.com/hedgetohedge/three.jpg",
     "text": "Hedge-to-Hedge"
   },
   {
     "name": "industrials",
-    "main": "jpg",
-    "one": "jpg",
-    "two": "jpg",
-    "three": "jpg",
+    "main": "http://hedgviz.s3.amazonaws.com/industrials/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/industrials/one.jpg",
+    "two": "http://hedgviz.s3.amazonaws.com/industrials/two.jpg",
+    "three": "http://hedgviz.s3.amazonaws.com/industrials/three.jpg",
     "text": "industrials"
   },
   {
     "name": "restaurants",
-    "main": "jpg",
-    "one": "mp4",
-    "two": "mp4",
-    "three": "mp4",
+    "main": "http://hedgviz.s3.amazonaws.com/restaurants/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/restaurants/one.mp4",
+    "two": "http://hedgviz.s3.amazonaws.com/restaurants/two.mp4",
+    "three": "http://hedgviz.s3.amazonaws.com/restaurants/three.mp4",
     "text": "restaurants"
   },
   {
     "name": "retail",
-    "main": "jpg",
-    "one": "jpg",
-    "two": "jpg",
-    "three": "jpg",
+    "main": "http://hedgviz.s3.amazonaws.com/retail/main.jpg",
+    "one": "http://hedgviz.s3.amazonaws.com/retail/one.jpg",
+    "two": "http://hedgviz.s3.amazonaws.com/retail/two.jpg",
+    "three": "http://hedgviz.s3.amazonaws.com/retail/three.jpg",
     "text": "retail"
   }
 ];
@@ -101,38 +101,40 @@ var showList = [
 var screenList = [
   {
     "number": "1",
-    "size": "large",
-    "type": "mp4",
-    "src": "main",
+    "name": "main",
+    "size": "large"
   },
   {
     "number": "2",
-    "size": "medium",
-    "type": "mp4",
-    "src": "one"
+    "name": "one",
+    "size": "medium"
   },
   {
     "number": "3",
+    "name": "logo",
     "size": "medium",
-    "type": "jpg",
-    "src": "HE_logo"
+    "src": "HE_logo.jpg"
   },
   {
     "number": "4",
-    "size": "medium",
-    "type": "mp4",
-    "src": "two"
+    "name": "two",
+    "size": "medium"
   },
   {
     "number": "5",
+    "name": "three",
     "size": "medium",
-    "type": "mp4",
-    "src": "three"
   },
   {
     "number": "6",
+    "name": "ticker",
     "size": "large",
-    "type": "text",
-    "src": 'MACRO MESSAGE'
+    "src": ''
+  },
+  {
+    "number": "7",
+    "name": "multi",
+    "size": "large",
+    "src": '/multiloop.gif'
   }
 ];
