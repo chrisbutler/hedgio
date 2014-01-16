9e259b6bb9f19210eb7d83613b7137c4a984476d
Chris Butler <chris@platformthirteen.com>
2014-01-16 00:27:07 -0500

    update defaults

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A simple app for displaying media content in pre-sized slices
 
 TODO
 
+video thumbnails (http://vdemedes.github.io/thumbbot/)
 check image dimensions/ratio
 server sessions for remote control
 use cropper 
--- collections/screens.js
+++ collections/screens.js
@@ -9,24 +9,45 @@ screenList = [
     "src": "http://hedgviz.s3.amazonaws.com/default/main.mp4"
   },
   {
+    "code": "GEN2",
+    "name": "charts",
+    "sector": "generic",
+    "type": "video",
+    "src": "http://hedgviz.s3.amazonaws.com/default/one.mp4"
+  },
+  {
+    "code": "GEN3",
+    "name": "numbers",
+    "sector": "generic",
+    "type": "video",
+    "src": "http://hedgviz.s3.amazonaws.com/default/two.mp4"
+  },
+  {
+    "code": "GEN4",
+    "name": "panning charts",
+    "sector": "generic",
+    "type": "video",
+    "src": "http://hedgviz.s3.amazonaws.com/default/three.mp4"
+  },
+  {
     "code": "MAC1",
     "name": "globe pan",
     "sector": "macro",
     "type": "video",
-    "src": "http://hedgviz.s3.amazonaws.com/default/one.mp4"
+    "src": "http://hedgviz.s3.amazonaws.com/macro/one.mp4"
   },
   {
     "code": "MAC2",
     "name": "globe scope",
     "sector": "macro",
     "type": "video",
-    "src": "http://hedgviz.s3.amazonaws.com/default/two.mp4"
+    "src": "http://hedgviz.s3.amazonaws.com/macro/two.mp4"
   },
   {
     "code": "MAC3",
     "name": "globe spin",
     "sector": "macro",
     "type": "video",
-    "src": "http://hedgviz.s3.amazonaws.com/default/three.mp4"
+    "src": "http://hedgviz.s3.amazonaws.com/macro/three.mp4"
   },
 ];
